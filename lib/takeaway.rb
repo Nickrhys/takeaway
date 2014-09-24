cd55a57d742f84eea0739075cb2d391aa4097f67
Nickrhys <nickrhys@hotmail.com>
2014-09-24 19:22:03 +0100

    takeaway tested and passsed

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -12,4 +12,8 @@ class Takeaway
 		orders << order
 	end
 
+	def order_message(customer, order)
+		"Customer: #{customer.name} \n Total: #{order.total} \n Your order will be delivered by #{order.time_ordered.hour + 1 }: #{order.time_ordered.min}"
+	end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,11 +3,10 @@ require "dish"
 
 describe Takeaway do
 	
-	let (:takeaway) {Takeaway.new(@menu)	}
-	let (:order) 	{double :order 			}
+	let (:takeaway) {Takeaway.new(@menu)							 }
+	let (:order) 	{double :order, total: 14, time_ordered: Time.now }
 
 	context 'functionality' do
-
 
 		it 'can be initialized with menu' do
 			takeaway = Takeaway.new [:dish]
@@ -22,5 +21,15 @@ describe Takeaway do
 			takeaway.add(order)
 			expect(takeaway.orders).to eq [order]
 		end
+
 	end
+
+	context 'taking an order' do
+
+		it 'can take and order from a customer' do
+			Nick = double :customer, name: "Nick"
+			expect(takeaway.order_message(Nick, order)).to eq "Customer: Nick \n Total: 14 \n Your order will be delivered by #{Time.now.hour + 1 }: #{Time.now.min}"
+		end
+	end
+
 end
